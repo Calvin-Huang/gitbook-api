e5ae6037a0a9a36795e61acc92f7376033afa76a
Calvin <calvin.peak@capslock.tw>
2016-10-07 17:01:57 +0900

    Remove webwork mock, this repo need to depends on gitbook service. Therefore, data source should comes from remote.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,9 @@
 require 'rspec'
 require 'rack/test'
-require 'webmock/rspec'
-require 'omniauth'
-require 'gitbook_api-gitbook'
+# require 'webmock/rspec'
+require 'gitbook_api'
 
 RSpec.configure do |config|
-  config.include WebMock::API
+  # config.include WebMock::API
   config.include Rack::Test::Methods
-  config.extend OmniAuth::Test::StrategyMacros, type: :strategy
 end
