782682f78f318be72fb7c75ed93a00f67cecc59b
Calvin <calvin.peak@capslock.tw>
2016-10-07 18:57:40 +0900

    Fix test, use hash map instead of json, json data is only response by controller or otherwhere.

--- spec/gitbook_api/api_spec.rb
+++ spec/gitbook_api/api_spec.rb
@@ -6,20 +6,17 @@ describe GitBook::API do
   describe GitBook::API::Stars do
     describe '#list' do
       it "should request user's stars and convert to json format" do
-        stars = GitBook.api.stars.list tester_username
-        expected_json = [
+        stars = GitBook.api.stars tester_username
+        expected_data = [
             {
                 id: 'test',
                 author: 'calvin-huang',
                 title: 'Test book for GitBookReader',
-                description:
-'iOS repo - https://github.com/Calvin-Huang/GitbookReader-iOS
- Rails repo - https://github.com/Calvin-Huang/GitbookReader
- This repo will not remove for unit test,',
+                description: "iOS repo - https://github.com/Calvin-Huang/GitbookReader-iOS\r\nRails repo - https://github.com/Calvin-Huang/GitbookReader\r\nThis repo will not remove for unit test,",
             }
-        ].to_json
+        ]
 
-        expect(stars).to eq(expected_json)
+        expect(stars).to eq(expected_data)
       end
     end
   end
